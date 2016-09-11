dcc8da53475ef353a47c80a89887982991b7d19c
Jonny <jonny@HDC-WRK-M19.local>
2016-09-11 11:27:26 -0700

    partial modification of app (nonfunctional)

---
+++ src/your-code-goes-here/data/cities.js
@@ -0,0 +1,47 @@
+
+export default [
+    {
+        id: '1',
+        name: 'Vancouver',
+    },
+    {
+        id: '2',
+        name: 'Victoria',
+    },
+    {
+        id: '3',
+        name: 'Seattle',
+    },
+    {
+        id: '4',
+        name: 'Portland',
+    },
+    {
+        id: '5',
+        name: 'Sacramento',
+    },
+    {
+        id: '6',
+        name: 'San Francisco',
+    },
+    {
+        id: '7',
+        name: 'Oakland',
+    },
+    {
+        id: '8',
+        name: 'San Jose',
+    },
+    {
+        id: '9',
+        name: 'L.A.',
+    },
+    {
+        id: '10',
+        name: 'San Diego',
+    },
+    {
+        id: '11',
+        name: 'Tijuana',
+    }
+];
--- src/your-code-goes-here/routes.js
+++ src/your-code-goes-here/routes.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Route } from 'react-router';
 
 import AppRoot from './containers/AppRoot';
-import People from './containers/people';
-import Person from './containers/person';
+import Cities from './containers/cities';
+import City from './containers/city';
 
 
 export default (
   <Route path="/" component={AppRoot}>
-    <Route path="people" component={People} />
-    <Route path="people/:id" component={Person} />
+    <Route path="cities" component={Cities} />
+    <Route path="city/:id" component={City} />
   </Route>
 );
