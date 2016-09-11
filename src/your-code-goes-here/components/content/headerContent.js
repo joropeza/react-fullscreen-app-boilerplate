8282ee0490c2f11ba43f9999a30021191f13ac9d
Jonny <jonny@HDC-WRK-M19.local>
2016-09-11 12:16:50 -0700

    code cleanup

--- src/your-code-goes-here/components/content/headerContent.js
+++ src/your-code-goes-here/components/content/headerContent.js
@@ -5,32 +5,33 @@ export default class HeaderContent extends Component {
     render() {
         const { route } = this.props;
         return (
-            <nav className="navbar navbar-default">
-        <div className="container-fluid">
-          <div className="navbar-header">
-            <button type="button"
-              className="navbar-toggle collapsed"
-              data-toggle="collapse"
-              data-target="#navbar"
-              aria-expanded="false"
-              aria-controls="navbar"
-            >
-              <span className="sr-only">Toggle navigation</span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-            </button>
-            <a className="navbar-brand" href="/">Fullscreen App</a>
-          </div>
-          <div id="navbar" className="navbar-collapse collapse">
-            <ul className="nav navbar-nav">
-              <li className={route === 'HOME' ? 'active' : null}><Link to={'/'}>Home</Link></li>
-              <li><Link to={'/people'}>Peeps</Link></li>
-              <li><Link to={'/people/1'}>Me</Link></li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+           <nav className="navbar navbar-default">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <button
+                          type="button"
+                          className="navbar-toggle collapsed"
+                          data-toggle="collapse"
+                          data-target="#navbar"
+                          aria-expanded="false"
+                          aria-controls="navbar"
+                        >
+                            <span className="sr-only">Toggle navigation</span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                        </button>
+                        <a className="navbar-brand" href="/">Fullscreen App</a>
+                    </div>
+                    <div id="navbar" className="navbar-collapse collapse">
+                        <ul className="nav navbar-nav">
+                            <li className={route === 'HOME' ? 'active' : null}><Link to={'/'}>Home</Link></li>
+                            <li><Link to={'/people'}>Peeps</Link></li>
+                            <li><Link to={'/people/1'}>Me</Link></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         );
     }
 }
