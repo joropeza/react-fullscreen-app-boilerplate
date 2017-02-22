ebecfd510644a5ba7a0ee5287be068151b24a5a3
Jonny <joropeza@gmail.com>
2017-02-21 18:03:50 -0800

    make hotreloading just, you know, work

--- src/your-code-goes-here/components/content/homePageContent.js
+++ src/your-code-goes-here/components/content/homePageContent.js
@@ -7,7 +7,7 @@ const HomePageContent = () => {
         <div className="inner cover">
             <h1 className="cover-heading">Full-screen single page app</h1>
             <p className="lead">
-            Full-screen app uses react and redux router to create a full screen experience.
+            Full-screen app uses react and redux router to create a full screen experience. And hot reloading.
         </p>
             <p className="lead">
                 <a href="https://github.com/joropeza/react-fullscreen-app-boilerplate" className="btn btn-secondary">Visit Repo</a>
--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,40 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'eval',
-  entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    'react-hot-loader/patch',
-    './src/boilerplate/index'
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'src')
-    }]
-  }
+    entry: [
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/only-dev-server',
+        'react-hot-loader/patch',
+        './src/boilerplate/index',
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/static/',
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(),
+        new webpack.NoEmitOnErrorsPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                use: ['babel-loader'],
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    devServer: {
+        host: 'localhost',
+        port: 3000,
+
+        historyApiFallback: true,
+        // respond to 404s with index.html
+
+        hot: true,
+        // enable HMR on the server
+    },
 };
